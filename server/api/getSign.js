4eca353ef2188e6fd8975842589a4dddc9221cf2
lijiye <lijiyebest@gmail.com>
2024-09-08 01:50:44 +0800

    feat: 签名算法

--- server/api/getSign.js
+++ server/api/getSign.js
@@ -55,25 +55,29 @@ function raw(args) {
 }
 
 module.exports = getSign = (params, res) => {
-  /**
-   * 签名算法
-   * 签名生成规则如下：
-   * 参与签名的字段包括noncestr（ 随机字符串）,
-   * 有效的jsapi_ticket, timestamp（ 时间戳）,
-   * url（ 当前网页的URL， 不包含# 及其后面部分）。
-   * 对所有待签名参数按照字段名的ASCII 码从小到大排序（ 字典序） 后，
-   *  使用URL键值对的格式（ 即key1 = value1 & key2 = value2…） 拼接成字符串string1。
-   * 这里需要注意的是所有参数名均为小写字符。 对string1作sha1加密， 字段名和字段值都采用原始值， 不进行URL 转义。
-   */
   var ret = {
     jsapi_ticket: params.ticket,
     nonceStr: createNonceStr(),
     timestamp: createTimestamp(),
     url: params.url,
   };
+
   console.log(params, ret);
-  var string = raw(ret);
-  ret.signature = sha1(string);
+
+  var oriArray = new Array();
+  oriArray[0] = params.nonceStr;
+  oriArray[1] = params.timestamp;
+  oriArray[2] = params.ticket;
+  oriArray.sort();
+
+  var original = oriArray.join("");
+  ret.signature = sha1(original);
+
+  if (params.signature) {
+    console.log("来自微信的signature---生成", params.signature, ret.signature);
+    res.send(params);
+    return
+  }
   ret.appId = config.appid;
   console.log("ret", ret);
   res.send(ret);
